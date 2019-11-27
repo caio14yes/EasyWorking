1a661f45b66978edad7e7e8986668cc5781fe414
Caio Henrique <caio14yes@gmail.com>
2019-11-27 10:30:04 -0300

    pra ver se funcionasabosta

--- krai/src/components/Button/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function Button(props){
-    return (
-        <button className={props.className} onClick={props.onClick}>{props.text}</button>
-    );
-}
