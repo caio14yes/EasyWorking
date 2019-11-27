9dcb0e5675facc6c894c39162135c867613a1a16
KEITH CASSIA MAKINO <0220831711019@fatectq.local>
2019-11-27 09:00:47 -0300

    arquivos

---
+++ src/components/Input/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Input(props){
+    return (
+        <input
+            id={props.id}
+            type={props.type} 
+            placeholder={props.placeholder}
+            onChange={props.onChange}
+            value={props.value}
+            />
+    );
+}
---
+++ src/components/Listar/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import FooFormCadastroCurriculoter from '../../FormCadastroCurriculo';
+
+class FormListarCurriculo extends FormCadastroCurriculo{
+
+  
+    render() {
+        let erros = this.state.validateErros
+        return (
+            <div>
+               
+            </div>
+        );
+    }
+}
+
+export default FormListarCurriculo;
